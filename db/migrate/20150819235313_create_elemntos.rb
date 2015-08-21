2314e857b2b010dde781a7f478667fd545ac50fe
hardmarlindart <mardem_lincow@yahoo.com.br>
2015-08-21 15:07:48 -0300

    segundo commit

--- db/migrate/20150819235313_create_elemntos.rb
+++ db/migrate/20150819235313_create_elemntos.rb
@@ -3,9 +3,16 @@ class CreateElemntos < ActiveRecord::Migration
     create_table :elemntos do |t|
       t.string :alcunha
       t.string :crime
-      
+      t.string :cidade_atuacao
+      t.string :zona_atuacao
+      t.string :string
+      t.string :motivo_condenacao
+      t.string :respode_crime
+      t.string :motivo_respode
 
       t.timestamps null: false
     end
   end
 end
+
+
